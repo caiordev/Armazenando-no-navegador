7e0660b6e86176d8f6cb5f0420dc5df2c1d7859a
caiordev <caioreisdev@gmail.com>
2023-03-24 19:18:10 -0300

    armazenando no navegador

---
+++ index.js
@@ -0,0 +1,45 @@
+document.getElementById("sessionBtn").addEventListener("click", function () {
+  const input = document.getElementById("session");
+  sessionStorage.setItem("info", input.value);
+  input.value = "";
+});
+
+document.getElementById("readSession").addEventListener("click", function () {
+  const info = sessionStorage.getItem("info");
+  alert("a informação salva é " + info);
+});
+
+//Local Storage
+document.getElementById("localBtn").addEventListener("click", function () {
+  const input = document.getElementById("local");
+  localStorage.setItem("text", input.value);
+  input.value = "";
+});
+
+document.getElementById("readLocal").addEventListener("click", function () {
+  const text = localStorage.getItem("text");
+  alert("o texto salvo no local storage é: " + text);
+});
+
+//Cookie
+document.getElementById("cookieBtn").addEventListener("click", function () {
+  const input = document.getElementById("cookie");
+  //cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = "info=" + input.value + ";";
+  const expiration = "expires=" + new Date(2022, 9, 9) + ";";
+  const path = "path=/;";
+  document.cookie = cookie + expiration + path;
+  input.value = "";
+  console.log(document.cookie);
+});
+
+document.getElementById("cookie2Btn").addEventListener("click", function () {
+  const input = document.getElementById("cookie2");
+  //cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = "text=" + input.value + ";";
+  const expiration = "expires=" + new Date(2023, 9, 9) + ";";
+  const path = "path=/;";
+  document.cookie = cookie + expiration + path;
+  input.value = "";
+  console.log(document.cookie);
+});
